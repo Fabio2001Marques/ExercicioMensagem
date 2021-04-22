e764dca3c3c86718cd876e9f37d5a8a36dfb8cc8
Fabio Marques <fabio2001marques@gmail.com>
2021-04-22 22:00:32 +0100

    Adicionar o evento onclick no butão e aparecer mensagem noutra janela

--- app/src/main/java/pt/ipg/exercicio1/MainActivity.kt
+++ app/src/main/java/pt/ipg/exercicio1/MainActivity.kt
@@ -1,8 +1,10 @@
 package pt.ipg.exercicio1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun enviaMensagem(view: View) {}
+    fun enviaMensagem(view: View) {
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMessage)
+
+        val mensagem = editTextMensagem.text.toString()
+
+        val intent = Intent(this, MostraMensagemActivity::class.java)
+        intent.putExtra("Mensagem", mensagem)
+
+        startActivity(intent)
+    }
 }
