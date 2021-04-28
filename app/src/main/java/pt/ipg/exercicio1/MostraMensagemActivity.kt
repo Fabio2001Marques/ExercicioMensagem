9ae1a16150553e8062aad9b27b9273422be5b0aa
Fabio Marques <fabio2001marques@gmail.com>
2021-04-28 09:40:19 +0100

    Formatar a hora para "HH:mm:ss"

--- app/src/main/java/pt/ipg/exercicio1/MostraMensagemActivity.kt
+++ app/src/main/java/pt/ipg/exercicio1/MostraMensagemActivity.kt
@@ -3,6 +3,7 @@ package pt.ipg.exercicio1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MostraMensagemActivity : AppCompatActivity() {
@@ -17,8 +18,10 @@ class MostraMensagemActivity : AppCompatActivity() {
         val textViewMensagem = findViewById<TextView>(R.id.textViewMensagem)
         textViewMensagem.setText(mensagem)
 
+        // Mostrar a data
         val textViewHora = findViewById<TextView>(R.id.textViewHora)
-        textViewHora.setText(data.toString())
+        val df = SimpleDateFormat("HH:mm:ss")
+        textViewHora.setText(df.format(data))
 
 
 
